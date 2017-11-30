67b6d93fbef365329534f2421d20ebd7f2bf70a6
Robert Hayman <rhayman@sitehands.com>
2017-11-30 13:15:21 -0500

    Adding structure for the game module

---
+++ web/modules/custom/game/src/Controller/Dashboard.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rhayman
+ * Date: 11/30/17
+ * Time: 1:13 PM
+ */
+
+
+namespace Drupal\game\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Render\HtmlResponse;
+use Drupal\user\Entity\User;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Drupal\customerportal\Plugin\LookupData;
+
+class Dashboard extends ControllerBase
+{
+	public function content(){
+	
+	}
+}
